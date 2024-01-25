219a3cd5250c68584636055a55a2cdf721f9df86
= <=>
2024-01-25 11:01:59 -0600

    Add show routes.

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-
-})
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {})
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -22,6 +20,10 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds:id', async (req, res) => {
+  res.render('campgrounds/show')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
