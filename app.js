eef05a366425042ae4c514419e417980557874b5
= <=>
2024-01-25 08:52:43 -0600

    Add basic staging

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
