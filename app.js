9981eec2ba1ac3d91337babe2163066580f18cfb
= <=>
2024-01-26 10:44:47 -0600

    Add the ability to delete a campground. Use express method overrides.

--- app.js
+++ app.js
@@ -53,6 +53,14 @@ app.put('/campgrounds/:id', async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  // Add functionality to delete more stuff here later.
+  // For now, just delete the whole object.
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 app.set('view engine', 'ejs');
