27f11d321b5528eb5aafec80672ad79aedf74f23
= <=>
2024-03-25 10:44:53 -0500

    Break out error wrappers into separate 'utils' folder.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/EspressError');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
@@ -23,37 +26,37 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const camp_raw = await Campground.find({});
   const campgrounds = camp_raw.sort((a, b) => a.title.localeCompare(b.title));
   res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground })
 })
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground })
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 app.delete('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
@@ -61,6 +64,15 @@ app.delete('/campgrounds/:id', async (req, res) => {
   // For now, just delete the whole object.
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds')
+})
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'Something went wrong.' } = err;
+  res.status(statusCode).send(message)
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
