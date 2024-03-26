7f9cc2c31b85634ea81f1d46c1cdbd322be3938e
= <=>
2024-03-26 11:06:41 -0500

    Add another layer of server-side validation.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/EspressError');
@@ -26,37 +27,60 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+
 app.get('/campgrounds', catchAsync(async (req, res) => {
   const camp_raw = await Campground.find({});
   const campgrounds = camp_raw.sort((a, b) => a.title.localeCompare(b.title));
   res.render('campgrounds/index', { campgrounds })
 }))
 
+
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
+
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+  // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required(),
+    }).required(),
+  })
+  const result = campgroundSchema.validate(req.body)
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  }
+  console.log(result);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`)
 }))
+
 
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground })
 })
 
+
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground })
 }))
+
 
 app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`)
 }))
+
 
 app.delete('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
@@ -66,16 +90,21 @@ app.delete('/campgrounds/:id', async (req, res) => {
   res.redirect('/campgrounds')
 })
 
+
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
 })
 
+
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong.' } = err;
-  res.status(statusCode).send(message)
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+  res.status(statusCode).render('error', { err })
 })
 
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
